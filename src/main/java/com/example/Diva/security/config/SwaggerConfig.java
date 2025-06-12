b93a429e5fc06aed3ef716cec9826dbecd68764e
BaSant <basssnthaider@gamil.com>
2025-06-12 03:11:19 +0300

    feat: add swagger config for API documentation

---
+++ src/main/java/com/example/Diva/security/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.example.Diva.security.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Your API Title")
+                        .version("1.0")
+                        .description("API with JWT authentication")
+                        .contact(new Contact()
+                                .name("Basant Haider")
+                                .email("basssnthaider@gmail.com")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+}
+}
