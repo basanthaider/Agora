b8dab72d7dfcd19a74b6f2b5bc92a375b0db4096
BaSant <basssnthaider@gamil.com>
2025-06-24 18:46:32 +0300

    feat: add validate annotation

--- src/main/java/com/example/Diva/domain/authentication/controller/AuthenticationController.java
+++ src/main/java/com/example/Diva/domain/authentication/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@Validated
 @SecurityRequirement(name = "bearerAuth")
 public class AuthenticationController {
     @Autowired
