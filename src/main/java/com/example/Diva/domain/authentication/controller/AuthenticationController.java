338692861cfddd4aee10e264dbe21656c2e70bf4
BaSant <basssnthaider@gamil.com>
2025-06-24 18:30:00 +0300

    fix: delete no needed imports

--- src/main/java/com/example/Diva/domain/authentication/controller/AuthenticationController.java
+++ src/main/java/com/example/Diva/domain/authentication/controller/AuthenticationController.java
@@ -3,10 +3,9 @@ package com.example.Diva.domain.authentication.controller;
 import com.example.Diva.domain.authentication.model.request.LoginRequestDto;
 import com.example.Diva.domain.authentication.model.request.LogoutRequestDto;
 import com.example.Diva.domain.authentication.model.request.RegisterRequestDto;
-import com.example.Diva.domain.authentication.service.implementation.AuthenticationService;
+import com.example.Diva.domain.authentication.service.implementation.AuthenticationServiceImpl;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @SecurityRequirement(name = "bearerAuth")
-@RequiredArgsConstructor
 public class AuthenticationController {
     @Autowired
-    private AuthenticationService authenticationService;
+    private AuthenticationServiceImpl authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<Object> register(@RequestBody RegisterRequestDto request) {
