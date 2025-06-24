c8de85d79656dd9d50d53ee4cb6e3c928f8fc60f
BaSant <basssnthaider@gamil.com>
2025-06-24 18:32:34 +0300

    feat:add validations

--- src/main/java/com/example/Diva/domain/authentication/model/request/LoginRequestDto.java
+++ src/main/java/com/example/Diva/domain/authentication/model/request/LoginRequestDto.java
@@ -1,10 +1,14 @@
 package com.example.Diva.domain.authentication.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
+    @Email(message = "Email should be valid")@NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
