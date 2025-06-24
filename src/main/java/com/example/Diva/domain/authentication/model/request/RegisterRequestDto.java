93c191445f4140bf664c75c02e083ce2478db272
BaSant <basssnthaider@gamil.com>
2025-06-24 18:43:44 +0300

    feat: add password regex

--- src/main/java/com/example/Diva/domain/authentication/model/request/RegisterRequestDto.java
+++ src/main/java/com/example/Diva/domain/authentication/model/request/RegisterRequestDto.java
@@ -3,6 +3,7 @@ package com.example.Diva.domain.authentication.model.request;
 import com.example.Diva.utill.BaseEnums;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -12,6 +13,8 @@ public class RegisterRequestDto {
     @Email(message = "Email should be valid")@NotBlank(message = "Email is required")
     private String email;
     @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_+=])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
     private String password;
     @NotBlank(message = "Confirm Password is required")
     private String confirmPassword;
