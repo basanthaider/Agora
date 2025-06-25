e6996733224ede91037c7c30c5318f0309404f26
BaSant <basssnthaider@gamil.com>
2025-06-25 23:20:43 +0300

    fix: update brand entity and add relationship with product

--- src/main/java/com/example/Diva/entity/Brand.java
+++ src/main/java/com/example/Diva/entity/Brand.java
@@ -1,15 +1,23 @@
 package com.example.Diva.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Builder
 public class Brand extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
-    private List<Image> images;
+    private List<Product> products = new ArrayList<>();
 }
