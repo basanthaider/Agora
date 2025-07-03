ad6b89f3e2bf2c1fe41385cfbff37b8742f8ff0a
BaSant <basssnthaider@gamil.com>
2025-07-03 22:29:26 +0300

    feat: calc the total cart price and init an empty CartItem list

--- src/main/java/com/example/Diva/entity/Cart.java
+++ src/main/java/com/example/Diva/entity/Cart.java
@@ -1,18 +1,38 @@
 package com.example.Diva.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Cart extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @OneToOne
     private User user;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
-    private List<CartItem> items;
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true,fetch = FetchType.EAGER)
+    private List<CartItem> items= new ArrayList<>();
+
+    public <E> Cart(User user, ArrayList<E> es) {
+        super();
+    }
+
+
+    public double getTotalPrice() {
+        return items.stream()
+                .mapToDouble(CartItem::getTotalPrice)
+                .sum();
+    }
+
 }
--- src/main/java/com/example/Diva/entity/CartItem.java
+++ src/main/java/com/example/Diva/entity/CartItem.java
@@ -1,7 +1,16 @@
 package com.example.Diva.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class CartItem extends BaseEntity {
     @Id
@@ -13,6 +22,9 @@ public class CartItem extends BaseEntity {
 
     @ManyToOne
     private ProductVariant variant;
-
+    @Min(1)
     private Integer quantity;
+    public double getTotalPrice() {
+        return variant.getPrice() * quantity;
+    }
 }
