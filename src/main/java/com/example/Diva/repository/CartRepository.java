e93bc8f53cea509877e0459c813460045a7adc35
BaSant <basssnthaider@gamil.com>
2025-07-03 22:33:20 +0300

    feat:add a jpa method

--- src/main/java/com/example/Diva/repository/CartRepository.java
+++ src/main/java/com/example/Diva/repository/CartRepository.java
@@ -1,9 +1,11 @@
 package com.example.Diva.repository;
 
 import com.example.Diva.entity.Cart;
+import com.example.Diva.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
+import java.util.Optional;
 
-    // Additional query methods can be defined here if needed
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUser(User currentUser);
 }
